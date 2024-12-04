54974075a53eed8cb6c1e30039661a56fd2bcc2b
Kritika Jain <kritikajain614@gmail.comgit config --global user.email kritikajain614@gmail.comgit config --global user.email kritikajain614@gmail.com>
2024-12-04 12:04:04 +0530

    [Kritika] Add :Calculate the wages based on Part Time or Full Time

--- Employee.js
+++ Employee.js
@@ -8,3 +8,32 @@ if(employee < 0.5){
     console.log("Present");
     return "Present";    
 }
+
+    const partTimeHours = 4;
+    const fullTimeHours = 8;
+    const wagesPerHours = 20;
+    
+    // Generate the random number between 0 to 2
+    let employeeType = Math.floor(Math.random() * 3);
+    let workedHours = 0;
+    
+    switch(employeeType){
+        case 0:
+            workedHours = 0;
+            console.log("No Time worked");
+            break;
+        case 1:
+            workedHours = partTimeHours;
+            console.log("Part Time Worked");
+            break;
+        case 2:
+            workedHours = fullTimeHours;
+            console.log("Full Time Worked");
+            break;
+        default:
+            workedHours = 0;
+            console.log("Invalid Output");
+    }
+    
+    let dailyWages = workedHours * wagesPerHours;
+    return dailyWages;  // count the wages .
